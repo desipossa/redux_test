b1d681a23379807d3219b4d359e9c8b21306c934
lee <sipossa@naver.com>
2022-10-14 16:30:23 +0900

    20221014

--- src/store.js
+++ src/store.js
@@ -26,10 +26,12 @@ const createInput = createSlice({
     name: '포함한 값받기',
     initialState: [],
     reducers: {
-        //immer는 어떻게 작동하는가???
+        //immer는 어떻게 작동하는가??? 객체 불변성을 무시하는 방법... 
+        //immer사용상의 규약 같은 거네... {} 를 써줘야 불변성을 무시하고 작동함...
         //create: (state, action) => { state.push(action.payload) }
         //create: (state, action) => [...state, action.payload]
-        create: (state, action) => { return [...state, action.payload] }
+        //create: (state, action) => { return [...state, action.payload] }
+        create: (state, action) => { state.push(action.payload) }
     }
 })
 
